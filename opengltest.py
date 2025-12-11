312337134202dbb037661a491d6860a2d966f5df
Dafydd Carver-Williams <d.carverwilliams@gmail.com>
2025-12-11 16:16:01 +0000

    began opengl testing

---
+++ opengltest.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.locals import *
+import math
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+phi = (1+math.sqrt(5))/2
+
+verticies = (
+    (0, 1, phi),
+    (0, -1, phi),
+    (phi, 0, 1),
+    (-phi, 0, 1)
+    )
+
+edges = (
+    (0,1),
+    (1,2),
+    (2,3),
+    (3,0),
+    )
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0,-5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1,0,1,0)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
