75863f524541ef6ef86b05a1e3b186757e5eeca8
Dafydd Carver-Williams <d.carverwilliams@gmail.com>
2025-12-11 21:14:52 +0000

    GOT A FUCKING PLANET WORKING

--- opengltest.py
+++ opengltest.py
@@ -1,28 +1,75 @@
 import pygame
 from pygame.locals import *
-import math
+import math as m
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-phi = (1+math.sqrt(5))/2
+phi = (1+m.sqrt(5))/2
+tilt_offset = m.degrees(m.atan(1/2))
 
-verticies = (
-    (0, 1, phi),
-    (0, -1, phi),
-    (phi, 0, 1),
-    (-phi, 0, 1)
-    )
+def Rx(v,angle):
+    new_y = m.cos(angle)*v[1] - m.sin(angle)*v[2]
+    new_z = m.sin(angle)*v[1] + m.cos(angle)*v[2]
+    return (v[0], new_y, new_z)
+
+#def Ry(v,angle):
+
+verticies = [
+    [0, -phi, 1],
+    [0, phi, 1],
+    [0, phi, -1],
+    [0, -phi, -1],
+    [1, 0, phi],
+    [-1, 0, phi],
+    [-1, 0, -phi],
+    [1, 0, -phi],
+    [phi, 1, 0],
+    [-phi, 1, 0],
+    [-phi, -1, 0],
+    [phi, -1, 0],
+    ]
+
+for i in range(len(verticies)):
+    verticies[i] = Rx(verticies[i], m.tan(1/2))
+
+print(verticies)
 
 edges = (
-    (0,1),
-    (1,2),
-    (2,3),
-    (3,0),
-    )
+    (0, 5),
+    (0, 4),
+    (0, 11),
+    (0, 3),
+    (0, 10),
+    (4, 5),
+    (4, 1),
+    (4, 8),
+    (4, 11),
+    (5, 1),
+    (5, 9),
+    (5, 10),
+    (8, 11),
+    (8, 2),
+    (8, 7),
+    (7, 2),
+    (7, 3),
+    (7, 6),
+    (7, 11),
+    (6, 2),
+    (6, 9),
+    (6, 3),
+    (6, 10),
+    (10, 3),
+    (10, 9),
+    (9, 1),
+    (9, 2),
+    (2, 1),
+    (1, 8),
+    (3, 11))
 
 
-def Cube():
+
+def Icosahedron():
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -37,7 +84,8 @@ def main():
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0,0.0,-5)
+
+    glTranslatef(0.0,0.0,-7)
 
     while True:
         for event in pygame.event.get():
@@ -45,9 +93,9 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1,0,1,0)
+        glRotatef(0.25,0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        Icosahedron()
         pygame.display.flip()
         pygame.time.wait(10)
 
